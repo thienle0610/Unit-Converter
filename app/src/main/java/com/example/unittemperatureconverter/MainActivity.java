2f9fc0bf64bfef611ab7f521313789d3254b5508
thienle0610 <67705332+thienle0610@users.noreply.github.com>
2021-01-30 22:15:36 -0800

    finish

--- app/src/main/java/com/example/unittemperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/unittemperatureconverter/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.unittemperatureconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button2;
+    EditText number;
+    TextView result;
+    Double temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number =(EditText) findViewById(R.id.textNumber);
+
+        button1 = (Button) findViewById(R.id.CtoF);
+
+        button2 = (Button) findViewById(R.id.FtoC);
+
+        result = (TextView) findViewById(R.id.resultText);
+
+        DecimalFormat format = new DecimalFormat("#.##");
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temp=Double.parseDouble(String.valueOf(number.getText()));
+                Double newTemp = temp*9/5+32;
+                result.setText(format.format(newTemp) + "°F");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temp=Double.parseDouble(String.valueOf(number.getText()));
+                Double newTemp = (temp-32)*5/9;
+                result.setText(format.format(newTemp) + "°C");
+            }
+        });
     }
 }
